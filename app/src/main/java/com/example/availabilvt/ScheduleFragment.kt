b2ed285c7dff8832bedcf3f80974103ebb45e1e8
Ria Vadhavkar <vadhavkar.ria@gmail.com>
2022-11-09 12:32:28 -0500

    added datepicker stuff to xml and kotlin so now it actually works

--- app/src/main/java/com/example/availabilvt/ScheduleFragment.kt
+++ app/src/main/java/com/example/availabilvt/ScheduleFragment.kt
@@ -10,19 +10,51 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.maps.MapView
+import android.app.DatePickerDialog
+import android.widget.EditText
+import java.util.*
+import java.util.Calendar;
+import android.app.TimePickerDialog;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 
 class ScheduleFragment : Fragment() {
+
+//    lateinit var date: EditText
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         var view = inflater.inflate(R.layout.fragment_schedule, container, false)
         var img = view.findViewById(R.id.buildingImage) as ImageView
+        var date = view.findViewById(R.id.datePicker) as EditText
         val button = view.findViewById(R.id.submitButton) as Button
 
         img.setImageResource(R.drawable.mcbryde)
 
+        date!!.setOnClickListener {
+
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(
+                view.getContext(), DatePickerDialog.OnDateSetListener
+                { view, year, monthOfYear, dayOfMonth ->
+                    date.setText("" + (monthOfYear + 1) + "/" + dayOfMonth + "/" + year)
+                },
+                year,
+                month,
+                day
+            )
+
+            datePickerDialog.show()
+        }
 
         button.setOnClickListener {
             findNavController().navigate(R.id.action_scheduleFragment_to_availabilityFragment)
